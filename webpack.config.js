5818a3c571f9721de8ff096bca6b433b38866fcf
Ashline Mpofu <ashline.m@gmail.com>
2016-09-06 23:21:16 +0200

    added browsersync

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
 var path = require('path');
-console.log(__dirname)
-    module.exports = {
-        entry: './app/index.js',
-        output: {
-            path: `${__dirname}/app/dist/`,
-            filename: 'index.js'
-        },
-        module: {
-            loaders: [
-                { test: path.join(__dirname, 'app'),
-                  loader: 'babel-loader', 
-		  query: {
-		  	presets: ['es2015']
-		  }
-		}
-            ]
-        }
-    };
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        path: `${__dirname}/app/dist/`,
+        filename: 'index.js'
+    },
+    module: {
+        loaders: [
+            { test: path.join(__dirname, 'app'),
+              loader: 'babel-loader',
+              query: {
+                  presets: ['es2015']
+              }
+          }
+        ]
+    },
+    plugins: [
+      new BrowserSyncPlugin({
+        // browse to http://localhost:3000/ during development,
+        // ./public directory is being served
+        host: 'localhost',
+        port: 3000,
+        server: { baseDir: ['app'] }
+      })
+    ]
+};
